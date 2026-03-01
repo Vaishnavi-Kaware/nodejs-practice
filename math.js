e0257d95aeebfb5d9a8e9025f7568b071ffa00e7
Vaishnavi Kaware <vkaware1212@gmail.com>
2026-03-01 13:03:13 +0530

    Export files

---
+++ math.js
@@ -0,0 +1,32 @@
+const sum =(a,b)=>a+b
+const mul=(a,b)=>a*b
+const PI = 3.14;
+const var1= "hello";
+
+//module.exports= 123;  //The second line overwrites the first one.
+
+module.exports={sum,mul,number:123};
+
+// module.exports= 123; //only one value
+
+// let obj={
+//   sum:sum,
+//   mul:mul,
+//   PI:PI,
+//   var1:var1,
+// };
+
+// module.exports=obj;
+
+
+module.exports={
+  sum:sum,
+  mul:mul,
+  PI:PI,
+  var1:var1,
+};
+
+module.exports.div=(a,b)=>a/b;
+
+exports.div=(a,b)=>a/b;
+//exports=5;//not correct
--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 
-console.log("NodeJS started 🚀");
+// console.log("NodeJS started 🚀");
+
+// console.log("hello");
+
+// console.log(process.argv);
+
+// const name=process.argv[2];
+// console.log("Hello "+ name);
+
+const someValue=require("./math")
+
+console.log(someValue.number);
+console.log(someValue. sum(4,5));
+console.log(someValue.mul(4,5));
+
+//console.log(someValue); //only one value
+
+const math=require("./math")
+console.log(math);
+console.log(math.sum(3,3));
+
+
+const math2=require("./math")
+console.log(math2.div(10,5));
+
+const math3=require("./math")
+console.log(math3.div(10,5));
