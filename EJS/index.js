ee948fe004195335a70a0ecfea4e9da2d07efbc5
Vaishnavi Kaware <vkaware1212@gmail.com>
2026-03-07 11:42:05 +0530

    EJS Started

---
+++ EJS/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app=express();
+const path=require("path")
+const port =8080;
+
+app.set('view engine',"ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.get("/",(req, res) =>{
+ // res.send("home")
+//  res.render("home.ejs")
+res.render("home")
+});
+
+app.listen(port, ()=>{
+  console.log(`conncetion started on port ${port}`);
+  
+})
